8b3d442b8887c0d6de5fb8dab66dd356aaa11db2
Devdtoo <devdtoo@gmail.com>
2019-12-28 23:56:31 +0530

    Add ScreenShot

---
+++ app/src/main/java/com/devdtoo/whatchat/Utility/Shared.java
@@ -0,0 +1,13 @@
+package com.devdtoo.whatchat.Utility;
+
+
+public class Shared {
+
+    public static final String sp_filename = "WhatChat";
+    public static final String sp_name = "name";
+    public static final String sp_passport_name = "passport_name";
+    public static final String sp_login_status = "login_status";
+    public static final String sp_email = "email";
+    public static final String sp_phone = "phone";
+
+}
